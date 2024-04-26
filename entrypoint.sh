4c4ed81a4d8a029727c131291325ae15a0bcf324
Ye Min Phyo <yeminphyo25@gmail.com>
2024-04-27 01:41:10 +0700

    modified entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
-git add -A && git commit -m "Update Feed"
+git status && git add -A && git commit -m "Update Feed"
 git push --set-upstream origin master
 
 echo "=================="
